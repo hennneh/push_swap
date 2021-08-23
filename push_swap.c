ae993a9e7fa092f6c6ebe307faab9c82185a961f
hennneh <hendrik.lehmann96@gmail.com>
2021-08-23 18:39:10 +0200

    adding double checker

--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include "push_swap.h"
 
+static int	check_double(int *stack_a, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			if (stack_a[i] == stack_a[j])
+				return (0)
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 static int	create_stack(int argc, char **argv, int *stack_a)
 {
 	int	i;
@@ -26,7 +46,8 @@ int	main(int argc, char **argv)
 {
 	int	len;
 	int	stack_a[argc - 1];
-	int stack_b[argc - 1];
+	int	stack_b[argc - 1];
+
 	if (!(len = create_stack(argc, argv, stack_a)))
 		write(1, "Error\n", 6);
 	return (0);
