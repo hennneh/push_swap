321eb9f16510ed5847d755fc3802d4f829262f0f
hennneh <hendrik.lehmann96@gmail.com>
2021-09-10 14:17:27 +0200

    initial checks

--- v2/push_swap.c
+++ v2/push_swap.c
@@ -14,20 +14,47 @@ void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-int	ft_error(void)
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_error_msg(void)
 {
 	ft_putstr_fd("Error\n", 2);
 	return (0);
 }
 
+int	ft_is_int(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		if (ft_strlen(argv[i] > 11))
+			return (0);
+											// finish input check for ints
+	}
+	return (1);
+}
+
 int	ft_init(int argc, char **argv)
 {
+	if (!ft_is_int(argc, argv))
+		return (0);
 	return (1);
 }
 
 int	main(int argc, char **argv)
 {
 	if (!ft_init(argc, argv))
-		return (ft_error());
+		return (ft_error_msg());
 	return (0);
 }
