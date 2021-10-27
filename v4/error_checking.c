12fc0184417601da13becafa5b89bf4d4a083fe9
hennneh <hendrik.lehmann96@gmail.com>
2021-10-27 13:08:45 +0200

    int checker

--- v4/error_checking.c
+++ v4/error_checking.c
@@ -16,15 +16,20 @@ int	ft_error(t_stacks *ptr_stacks)
 
 int	ft_isint(int argc, char **argv)
 {
-	int	nbr;
-	int	i;
+	long	nbr;
+	int		i;
 
 	i = 1;
 	while (i < argc)
 	{
 		if (ft_strlen(argv[i]) > 12)
 			return (1);
+		if (ft_strlen(argv[i]) == 11 && argv[i][0] != '-')
+			return (1);
 		nbr = ft_atol(argv[i]);
+		if (nbr > 2147483647 || nbr < -2147483648)
+			return (1);
+		return (0);
 	}
 }
 
