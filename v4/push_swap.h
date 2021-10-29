2c7048e484a0fd81add512f53468cced7a13cebc
hennneh <hendrik.lehmann96@gmail.com>
2021-10-29 17:49:30 +0200

    testing sort three

--- v4/push_swap.h
+++ v4/push_swap.h
@@ -23,5 +23,18 @@ int		ft_atoi(const char *nptr);
 int		ft_isdup(int argc, t_stacks *ptr_stacks);
 void	ft_sort(t_stacks *ptr_stacks);
 int		ft_issorted(t_stacks *ptr_stacks);
+void	sa(t_stacks *ptr_stacks);
+void	sb(t_stacks *ptr_stacks);
+void	ss(t_stacks *ptr_stacks);
+void	ss(t_stacks *ptr_stacks);
+void	pa(t_stacks *ptr_stacks);
+void	pb(t_stacks *ptr_stacks);
+void	ra(t_stacks *ptr_stacks);
+void	rb(t_stacks *ptr_stacks);
+void	rr(t_stacks *ptr_stacks);
+void	rra(t_stacks *ptr_stacks);
+void	rrb(t_stacks *ptr_stacks);
+void	rrr(t_stacks *ptr_stacks);
+void	ft_sort_three(t_stacks *ptr_stacks);
 
 #endif
--- v4/sort.c
+++ v4/sort.c
@@ -17,25 +17,64 @@ int	ft_issorted(t_stacks *ptr_stacks)
 	return (1);
 }
 
+void	ft_sort_three(t_stacks *ptr_stacks)
+{
+	if (ptr_stacks->stack_a[0] == 2 && ptr_stacks->stack_a[1] == 1)
+	{
+		ra(ptr_stacks);
+		ra(ptr_stacks);
+	}
+	else if (ptr_stacks->stack_a[0] == 0 && ptr_stacks->stack_a[1] == 2)
+	{
+		rra(ptr_stacks);
+		sa(ptr_stacks);
+	}
+	else if (ptr_stacks->stack_a[0] == 1 && ptr_stacks->stack_a[1] == 2)
+		rra(ptr_stacks);
+	else if (ptr_stacks->stack_a[0] == 1 && ptr_stacks->stack_a[1] == 0)
+		sa(ptr_stacks);
+	else if (ptr_stacks->stack_a[0] == 2 && ptr_stacks->stack_a[1] == 0)
+		ra(ptr_stacks);
+}
+
 void	ft_sort(t_stacks *ptr_stacks)
 {
 // testing
-	printf("this is inside of sort.c\n");
-        int     j = 0;
-        printf("this is stack a\n\n");
-        while (j < ptr_stacks->pos_a)
-        {
-                printf("%d\n", ptr_stacks->stack_a[j]);
-                j++;
-        }
-        printf("\n\nand this is stack c\n\n");
-        j = 0;
-        while (j < ptr_stacks->pos_a)
-        {
-                printf("%d\n", ptr_stacks->stack_c[j]);
-                j++;
-        }
+	printf("this is inside of sort.c\n");						//test
+        int     j = 0;											//test
+        printf("this is stack a\n\n");							//test
+        while (j < ptr_stacks->pos_a)							//test
+        {														//test
+                printf("%d\n", ptr_stacks->stack_a[j]);			//test
+                j++;											//test
+        }														//test
+        printf("\n\nand this is stack c\n\n");					//test
+        j = 0;													//test
+        while (j < ptr_stacks->pos_a)							//test
+        {														//test
+                printf("%d\n", ptr_stacks->stack_c[j]);			//test
+                j++;											//test
+        }														//test
 // end of test
 	if (ft_issorted(ptr_stacks))
 		return ;
+	if (ptr_stacks->pos_a == 3)
+		ft_sort_three(ptr_stacks);
+/*	else if (ptr_stacks->pos_a == 5)							//not implemented
+		ft_sort_five(ptr_stacks);								//not implemented
+	else if (ptr_stacks->pos_a == 100)							//not implemented
+		ft_sort_hundred(ptr_stacks);							//not implemented
+	else if (ptr_stacks->pos_a == 500)							//not implemented
+		ft_sort_fivehundred(ptr_stacks);						//not implemented
+	else														//not implemented
+		ft_sort_rest(ptr_stacks);*/								//not implemented
+	//another test												//test
+	printf("and now the sorted array\n");						//test
+	j = 0;														//test
+	while (j < ptr_stacks->pos_a)								//test
+	{															//test
+		printf("%d\n", ptr_stacks->stack_a[j]);					//test
+			j++;												//test
+	}															//test
+	//end of test												//test
 }
