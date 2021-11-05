35f8b13026f3763388ebada64ec56fb468d2a032
hennneh <hendrik.lehmann96@gmail.com>
2021-11-05 14:55:41 +0100

    testing approach for sort>5

--- v4/sort100.c
+++ v4/sort100.c
@@ -1,6 +1,20 @@
 #include "push_swap.h"
 
+static void	ft_push_half(t_stacks *ptr_stacks)
+{
+	int	i;
+
+	i = ptr_stacks->pos_a / 2;
+	while (i > 0)
+	{
+		
+	}
+}
+
 void	ft_sort_hundred(t_stacks *ptr_stacks)
 {
-
+	if (ptr_stacks->pos_a > 5)
+		ft_push_half(ptr_stacks);
+	if (ptr_stacks->pos_a > 5)
+		ft_sort_hundred(ptr_stacks);
 }
