176a7aaccb37b5845f4bbb5c0a310abc889c74fe
hennneh <hendrik.lehmann96@gmail.com>
2021-11-06 16:26:09 +0100

    finding head for > index

--- v4/sort100.c
+++ v4/sort100.c
@@ -1,21 +1,33 @@
 #include "push_swap.h"
 
-static void	ft_move_b(t_stacks *ptr_stacks)
+static int	ft_find_head(t_stacks *ptr_stacks)
 {
 	int	i;
+	int	head;
+	int	*count;
 
+	count = malloc(sizeof(int) * ptr_stacks->pos_a);
+	head = 0;
 	i = 1;
-	while (i < ptr_stacks->pos_a)
+	while (head < ptr_stacks->pos_a)
 	{
-		if (ptr_stacks->stack_a[i] < ptr_stacks->stack_a[i - 1])
-		{
-			ra(ptr_stacks);
-			pb(ptr_stacks);
-		}
+		count[head] = 1;
+		head++;
+	}
+	head = 0;
+	while (i != ptr_stacks->stack_a[head])
+	{
+		if (ptr_stacks->stack_a[i] > ptr_stacks->stack_a[head])
+			count[head]++;
+		i++;
+		if (i == ptr_stacks->pos_a)
+			i = 0;
 	}
 }
 
 void	ft_sort_hundred(t_stacks *ptr_stacks)
 {
-	ft_move_b(ptr_stacks);
+	int	i;
+
+	i = ft_find_head(ptr_stacks);
 }
