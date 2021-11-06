2b78efca8c5343b7f267ecf34b4e8c56beec16b0
hennneh <hendrik.lehmann96@gmail.com>
2021-11-06 15:57:29 +0100

    editing sort>5

--- v4/sort100.c
+++ v4/sort100.c
@@ -1,20 +1,21 @@
 #include "push_swap.h"
 
-static void	ft_push_half(t_stacks *ptr_stacks)
+static void	ft_move_b(t_stacks *ptr_stacks)
 {
 	int	i;
 
-	i = ptr_stacks->pos_a / 2;
-	while (i > 0)
+	i = 1;
+	while (i < ptr_stacks->pos_a)
 	{
-		
+		if (ptr_stacks->stack_a[i] < ptr_stacks->stack_a[i - 1])
+		{
+			ra(ptr_stacks);
+			pb(ptr_stacks);
+		}
 	}
 }
 
 void	ft_sort_hundred(t_stacks *ptr_stacks)
 {
-	if (ptr_stacks->pos_a > 5)
-		ft_push_half(ptr_stacks);
-	if (ptr_stacks->pos_a > 5)
-		ft_sort_hundred(ptr_stacks);
+	ft_move_b(ptr_stacks);
 }
