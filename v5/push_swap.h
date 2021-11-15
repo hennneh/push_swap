a6c7ac157d44bbde1ced9eaabea36adb85eff263
hennneh <hendrik.lehmann96@gmail.com>
2021-11-15 14:41:57 +0100

    norming

--- v5/push_swap.h
+++ v5/push_swap.h
@@ -17,4 +17,13 @@ typedef struct s_s
 	int	hd;
 }	t_s;
 
+void	ft_free(t_s *ps);
+int		ft_error(t_s *ps);
+int		ft_isdup(int argc, t_s *ps);
+int		ft_isint(int argc, char **argv);
+int		ft_isnum(int argc, char **argv);
+long	ft_atol(char *s);
+size_t	ft_strlen(const char *str);
+int		ft_atoi(const char *nptr);
+
 #endif
---
+++ v5/utils.c
@@ -0,0 +1,58 @@
+#include "push_swap.h"
+
+long	ft_atol(char *s)
+{
+	long	nbr;
+	int		i;
+	int		sign;
+
+	nbr = 0;
+	i = 0;
+	sign = 1;
+	if (s[i] == '-')
+	{
+		i++;
+		sign = -1;
+	}
+	while (s[i])
+	{
+		nbr = nbr * 10 + s[i] - '0';
+		i++;
+	}
+	return (nbr * sign);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		sign;
+	int		num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((nptr[i] > 8 && nptr[i] < 14) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == 43 || nptr[i] == 45)
+	{
+		if (nptr[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] > 47 && nptr[i] < 58)
+	{
+		num = num * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (num * sign);
+}
