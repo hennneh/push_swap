86dd0ea0d225bc6f134b63b3107fccfc3ad773c9
hennneh <hendrik.lehmann96@gmail.com>
2021-11-24 21:36:42 +0100

    pushing half

--- v6/sort_rest.c
+++ v6/sort_rest.c
@@ -2,7 +2,49 @@
 
 static int	ft_psh_hlf(t_s *ps, int srt)
 {
+	int	rt;
+	int	min;
+	int	max;
+	int	i;
+	int	pshd;
+	int	mid;
 
+	rt = 0;
+	pshd = 0;
+	i = 0;
+	min = ps->a[0];
+	max = ps->a[0];
+	while (i < srt)
+	{
+		if (ps->a[i] > max)
+			max = ps->a[i];
+		if (ps->a[i] < min)
+			min = ps->a[i];
+		i++;
+	}
+	mid = (max - min) / 2 + min;
+	i = 0;
+	while (i < srt && pshd < ((max - min) / 2))
+	{
+		if (ps->a[0] < mid)
+		{
+			pb(ps);
+			pshd++;
+		}
+		else
+		{
+			ra(ps);
+			rt++;
+		}
+		i++;
+	}
+	i = 0;
+	while (i < rt)
+	{
+		rra(ps);
+		i++;
+	}
+	return (pshd);
 }
 
 static void	ft_srt(t_s *ps, int srt)
