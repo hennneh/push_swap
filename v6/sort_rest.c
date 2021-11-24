afcf3bfbebe158f9010aec29d3dedb7252f1b58f
hennneh <hendrik.lehmann96@gmail.com>
2021-11-24 23:53:38 +0100

    backsort algorhythm

--- v6/sort_rest.c
+++ v6/sort_rest.c
@@ -85,12 +85,65 @@ static void	ft_srt(t_s *ps, int srt)
 		}
 	}
 }
-/*
+
+static int	ft_psh_bck_hlf(t_s *ps, int pshd)
+{
+// need to write this function to push back half of the to be sorted elements on top of b to a !!!!!!!!!!!!!!!!!!!!!!!!!
+}
+
+static void	ft_bck_srt_three(t_s *ps)
+{
+	if (ps->b[0] > ps->b[1] && ps->b[1] > ps->b[2] && ps->b[0] > ps->b[2])
+		ft_thrtwoone(ps);
+	else if (ps->b[0] < ps->b[1] && ps->b[1] > ps->b[2] && ps->b[0] < ps->b[2])
+		ft_onethrtwo(ps);
+	else if (ps->b[0] < ps->b[1] && ps->b[1] > ps->b[2] && ps->b[0] > ps->b[2])
+		ft_twothrone(ps);
+	else if (ps->b[0] > ps->b[1] && ps->b[1] < ps->b[2] && ps->b[0] < ps->b[2])
+		ft_twoonethr(ps);
+	else if (ps->b[0] > ps->b[1] && ps->b[1] < ps->b[2] && ps->b[0] > ps->b[2])
+		ft_thronetwo(ps);
+	else if (ps->b[0] < ps->b[1] && ps->b[1] < ps->b[2] && ps->b[0] < ps->b[2])
+		ft_onetwothr(ps);
+}
+
+static void	ft_bck_srt(t_s *ps, int pshd)
+{
+	if (pshd == 1)
+		pa(ps);
+	else if (pshd == 2)
+	{
+		if (ps->b[0] > ps->b[1])
+		{
+			pa(ps);
+			pa(ps);
+		}
+		else
+		{
+			sb(ps);
+			pa(ps);
+			pa(ps);
+		}
+	}
+	else if (pshd == 3)
+		ft_bck_srt_three(ps)
+}
+
 static void ft_srt_bck(t_s *ps, int pshd)
 {
+	int	srt;
 
+	srt = 0;
+	if (pshd > 3)
+	{
+		srt = ft_psh_bck_hlf(ps, pshd);
+		ft_sort_rest(ps, srt);
+		ft_srt_bck(ps, (pshd - srt));
+	}
+	else
+		ft_bck_srt(ps, pshd);
 }
-*/
+
 void	ft_sort_rest(t_s *ps, int srt)
 {
 	int	pshd;
@@ -103,6 +156,6 @@ void	ft_sort_rest(t_s *ps, int srt)
 	}	
 	else
 		ft_srt(ps, srt);
-/*	if (pshd > 0)
-		ft_srt_bck(ps, pshd);*/
+	if (pshd > 0)
+		ft_srt_bck(ps, pshd);
 }
