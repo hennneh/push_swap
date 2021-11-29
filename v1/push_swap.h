ad63a960830488b541a67bab912b1b7e69771363
hennneh <hendrik.lehmann96@gmail.com>
2021-11-29 14:42:22 +0100

    final version

--- v1/push_swap.h
+++
@@ -1,8 +0,0 @@
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include <unistd.h>
-
-int	ft_atoi(const char *nptr);
-
-#endif
--- v3/push_swap.h
+++
@@ -1,9 +0,0 @@
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include <unistd.h>
-
-void	ft_putstr_fd(char *s, int fd);
-int		ft_strlen(char *s);
-
-#endif
